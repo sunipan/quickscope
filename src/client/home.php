363cb44985ddd31bdcafce8b26427db424f9193c
Sebi Unipan <sunipan@gmail.com>
2021-11-19 18:02:40 -0800

    finished how cards will look with PHP auto generation

--- src/client/home.php
+++ src/client/home.php
@@ -23,81 +23,41 @@
         </div>
       </div>
     </div>
-    <div class="col-lg-6 offset-lg-1">
-      <div class="header text-white">
-        <h2>Popular Posts</h2>
-      </div>
-      <div class="card col-lg-12">
-        <div class="card-body">
-          <h5 class="card-title">Card title</h5>
-          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-          <a href="#" class="btn btn-primary">Go somewhere</a>
-        </div>
-      </div>
-    </div>
-    <div class="col-lg-6 offset-lg-1">
-      <div class="card col-md-12">
-        <div class="card-body">
-          <h5 class="card-title">Card title</h5>
-          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-          <a href="#" class="btn btn-primary">Go somewhere</a>
-        </div>
-      </div>
-    </div>
-    <div class="col-lg-6 offset-lg-1">
-      <div class="card col-md-12">
-        <div class="card-body">
-          <h5 class="card-title">Card title</h5>
-          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-          <a href="#" class="btn btn-primary">Go somewhere</a>
-        </div>
-      </div>
-    </div>
-    <div class="col-lg-6 offset-lg-1">
-      <div class="card col-md-12">
-        <div class="card-body">
-          <h5 class="card-title">Card title</h5>
-          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-          <a href="#" class="btn btn-primary">Go somewhere</a>
-        </div>
-      </div>
-    </div>
-    <div class="col-lg-6 offset-lg-1">
-      <div class="card col-md-12">
-        <div class="card-body">
-          <h5 class="card-title">Card title</h5>
-          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-          <a href="#" class="btn btn-primary">Go somewhere</a>
-        </div>
-      </div>
-    </div>
-    <div class="col-lg-6 offset-lg-1">
-      <div class="card col-md-12">
-        <div class="card-body">
-          <h5 class="card-title">Card title</h5>
-          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-          <a href="#" class="btn btn-primary">Go somewhere</a>
-        </div>
-      </div>
-    </div>
-    <div class="col-lg-6 offset-lg-1">
-      <div class="card col-md-12">
-        <div class="card-body">
-          <h5 class="card-title">Card title</h5>
-          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-          <a href="#" class="btn btn-primary">Go somewhere</a>
-        </div>
-      </div>
-    </div>
-    <div class="col-lg-6 offset-lg-1">
-      <div class="card col-md-12">
-        <div class="card-body">
-          <h5 class="card-title">Card title</h5>
-          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-          <a href="#" class="btn btn-primary">Go somewhere</a>
-        </div>
-      </div>
-    </div>
+    <!-- Create fake posts by changing $i threshold -->
+    <?php
+    for ($i = 0; $i < 100; $i++) {
+      if ($i === 0) {
+        // Print the first post
+        echo '<div class="col-lg-6 offset-lg-1 mb-2">
+          <div class="header text-white">
+            <h2>Popular Posts</h2>
+          </div>
+          <div class="card col-lg-12">
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card\'s content.</p>
+              <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+            <img src="img/kermit.png" class="card-img-bottom" />
+          </div>
+        </div>';
+      } else {
+        // Print the rest of the posts
+        echo '<div class="mb-2">
+            <div class="col-lg-6 offset-lg-1">
+            <div class="card col-lg-12">
+              <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card\'s content.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+              </div>
+              <img src="img/kermit.png" class="card-img-bottom" />
+            </div>
+          </div>
+        </div>';
+      }
+    }
+    ?>
   </div>
 </body>
 
