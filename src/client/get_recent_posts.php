8e5cbf45b13a145d3c2c33f962ea7fe2edc76437
Sebi Unipan <sunipan@gmail.com>
2021-11-23 00:43:03 -0800

    almost finished home page, need to make dropdown for login avatar

---
+++ src/client/get_recent_posts.php
@@ -0,0 +1,9 @@
+<?php
+
+for ($i = 0; $i < 5; $i++) {
+  echo '<li class="list-group-item recent-post">
+  <a class="text-decoration-none text-dark fw-bold" href="#">
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+  </a>
+</li>';
+}
--- src/client/home.php
+++ src/client/home.php
@@ -8,89 +8,125 @@
   <meta name="viewport" content="width=device-width, initial-scale=1" />
 
   <link rel="stylesheet" href="css/style.css" />
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" integrity="sha512-Fo3rlrZj/k7ujTnHg4CGR2D7kSs0v4LLanw2qksYuRlEzO+tcaEPQogQ0KaoGN26/zrn20ImR1DfuLWnOo7aBA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
   <title>Quickscope - Forum For LEETS</title>
 </head>
 
 <body>
-  <div class="container post-container">
-    <div class="col-3 about-us-home d-none d-lg-block">
-      <h2 class="hide">placeholder</h2>
-      <div class="card">
-        <div class="card-body">
-          <h5 class="card-title text-center
-          ">Quickscope Information</h5>
-          <div class="row text-center">
-            <a class="col text-decoration-none text-secondary" href="about.html">
-              <p class="font-title-12 mt-3">About Us</p>
-            </a>
-            <a class="col text-decoration-none text-secondary" href="privacy.html">
-              <p class="font-title-12 mt-3">Privacy Policy</p>
-            </a>
-          </div>
-          <div class="row text-center">
-            <a class="col text-decoration-none text-secondary" href="terms.html">
-              <p class="font-title-12">Terms</p>
-            </a>
-            <a class="col text-decoration-none text-secondary" href="contact.html">
-              <p class="font-title-12">Contact</p>
-            </a>
-          </div>
-          <div class="row text-center">
-            <a class="col text-decoration-none text-secondary" href="allforums.php">
-              <p class="font-title-12">All Forums</p>
-            </a>
-            <a class="col text-decoration-none text-secondary" href="https://www.youtube.com/watch?v=Udvwea3YzZA">
-              <p class="font-title-12">Our Origins</p>
-            </a>
-          </div>
+  <div class="container-fluid post-container">
+    <div class="row">
+      <div class="col-lg-7 offset-lg-1">
+        <div class="header text-white">
+          <h2>Popular Posts</h2>
         </div>
-      </div>
-    </div>
-    <div class="col-3 create-a-forum d-none d-lg-block">
-      <div class="card">
-        <div class="card-body">
-          <h5 class="card-title text-center">Create a Forum</h5>
-        </div>
-      </div>
-    </div>
-  </div>
-  <!-- Create fake posts by changing $i threshold -->
-  <?php
-  for ($i = 0; $i < 10; $i++) {
-    if ($i === 0) {
-      // Print the first post
-      echo '<div class="col-lg-6 offset-lg-1 mb-2">
-          <div class="header text-white">
-            <h2>Popular Posts</h2>
-          </div>
-          <div class="card col-lg-12">
+        <!-- Create fake posts by changing $i threshold -->
+        <?php
+        for ($i = 0; $i < 10; $i++) {
+          if ($i === 0) {
+            // Print the first post
+            echo '<div class="card col-lg-12 mb-2">
             <div class="card-body">
               <h5 class="card-title">Card title</h5>
               <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card\'s content.</p>
-              <a href="#" class="btn btn-primary">Go somewhere</a>
+              <a href="#" class="btn btn-dark">Go somewhere</a>
             </div>
-            <img src="img/kermit.png" class="card-img-bottom" />
-          </div>
-        </div>';
-    } else {
-      // Print the rest of the posts
-      echo '<div class="mb-2">
-            <div class="col-lg-6 offset-lg-1">
-            <div class="card col-lg-12">
+            <div class="col-10 offset-1">
+              <img src="img/kermit.png" class="card-img-bottom" />
+            </div>
+            <div class="col-1 offset-1 comment-stuff d-flex mt-3">
+              <span style="font-size: 1.25rem;" class="comment-count">
+                <i class="fa fa-comment icon-2x"></i>
+                <span>0</span>
+              </span>
+            </div>
+          </div>';
+          } else {
+            // Print the rest of the posts
+            echo '<div class="card col-lg-12 mb-2">
               <div class="card-body">
                 <h5 class="card-title">Card title</h5>
                 <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card\'s content.</p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
+                <a href="#" class="btn btn-dark">Go somewhere</a>
               </div>
-              <img src="img/kermit.png" class="card-img-bottom" />
+              <div class="col-10 offset-1">
+                <img src="img/kermit.png" class="card-img-bottom" />
+              </div>
+              <div class="col-1 offset-1 comment-stuff d-flex mt-3">
+                <span style="font-size: 1.25rem;" class="comment-count">
+                  <i class="fa fa-comment icon-2x"></i>
+                  <span>0</span>
+                </span>
+              </div>
+            </div>';
+          }
+        }
+        ?>
+      </div>
+      <!-- Recent posts card -->
+      <div class="col-3 d-none d-lg-block">
+        <h2 class="hide">placeholder</h2>
+        <div class="card mb-3">
+          <div class="card-body">
+            <h5 class="card-title">Recent Posts</h5>
+            <ul class="list-group list-group-flush recent-list">
+              <?php include('get_recent_posts.php'); ?>
+          </div>
+        </div>
+        <!-- Create a forum card -->
+        <div class="card mb-3">
+          <div class="card-body">
+            <h5 class="card-title">Join or Create Your Own Community!</h5>
+            <div class="post-forum-buttons d-flex flex-column">
+              <a href="#" class="btn btn-primary mb-3">Create a Post</a>
+              <a href="#" class="btn btn-danger">Create a Forum</a>
             </div>
           </div>
-        </div>';
-    }
-  }
-  ?>
+        </div>
+        <div class="card about-us-home mb-3">
+          <div class="card-body">
+            <h5 class="card-title">Quickscope Information</h5>
+            <div class="row">
+              <a class="col text-decoration-none text-secondary" href="about.html">
+                <p class="font-title-12 mt-3">About Us</p>
+              </a>
+              <a class="col text-decoration-none text-secondary" href="privacy.html">
+                <p class="font-title-12 mt-3">Privacy Policy</p>
+              </a>
+            </div>
+            <div class="row">
+              <a class="col text-decoration-none text-secondary" href="terms.html">
+                <p class="font-title-12">Terms</p>
+              </a>
+              <a class="col text-decoration-none text-secondary" href="contact.html">
+                <p class="font-title-12">Contact</p>
+              </a>
+            </div>
+            <div class="row">
+              <a class="col text-decoration-none text-secondary" href="allforums.php">
+                <p class="font-title-12">All Forums</p>
+              </a>
+              <a class="col text-decoration-none text-secondary" href="https://www.youtube.com/watch?v=Udvwea3YzZA">
+                <p class="font-title-12">Our Origins</p>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
-  <script src="js/custom.js"></script>
+  <!-- This script contains all custom JS for styling this page -->
+  <script>
+    $(window).scroll(() => {
+      var pos = $(window).scrollTop();
+      console.log(pos);
+      if (pos > 680) {
+        $(".about-us-home").css("margin-top", pos - 660);
+      } else {
+        if ($(".about-us-home").css("margin-top") != "0px")
+          $(".about-us-home").css("margin-top", "0px");
+      }
+    });
+  </script>
 </body>
 
 </html>
