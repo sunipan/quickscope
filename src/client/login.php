e7f229fcf9932772ecb28a861f13722cca3d333f
AmritGoesCrazy <amritpalaujla204@gmail.com>
2021-11-24 23:08:03 -0800

    login page
    :)

---
+++ src/client/login.php
@@ -0,0 +1,51 @@
+<?php include('app.php'); ?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css" />
+
+    <style>
+        
+        body {
+    text-align: center;
+    }
+        form {
+    display: inline-block;
+    }
+    #logintitle{
+            margin-top: 8em;
+            color: white;
+            padding-bottom:0.5em;
+        }
+
+    
+
+
+        
+    </style>
+</head>
+
+<body>
+    
+
+    
+
+    <form name="login" action="loginprocess.php"> 
+        <h1 id="logintitle" for="login">Login</h1>
+    <div class="inputs">
+        <input type="text" placeholder="Username"></input>
+        <br>
+        <input type="text" placeholder="Password"></input>
+        <br><br>
+        <input type="submit"></input>
+    </div>
+  
+    </form>
+    
+</body>
+</html>
