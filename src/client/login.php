42a448b8db019ed21956c32059adb666399ff391
Sebi Unipan <sunipan@gmail.com>
2021-11-26 00:21:59 -0800

    just formatted files, add bootstrap styles to match rest of the site

--- src/client/login.php
+++ src/client/login.php
@@ -3,57 +3,53 @@
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/style.css" />
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="css/style.css" />
 
-    <style>
-        
-        body {
-    text-align: center;
-    }
-        form {
-    display: inline-block;
-    }
-    #logintitle{
-            margin-top: 17em;
-            color: white;
-            padding-bottom:0.5em;
-        }
-        h1{
-        font-size: large;
+  <style>
+    body {
+      text-align: center;
     }
 
-    a{
-        color: white;
-        font-size:small;
+    form {
+      display: inline-block;
     }
 
+    #logintitle {
+      margin-top: 17em;
+      color: white;
+      padding-bottom: 0.5em;
+    }
 
-        
-    </style>
+    h1 {
+      font-size: large;
+    }
+
+    a {
+      color: white;
+      font-size: small;
+    }
+  </style>
 </head>
 
 <body>
-    
-
-    
-
-    <form name="login" action="loginprocess.php"> 
-        <h1 id="logintitle" for="login">Login</h1>
+  <form name="login" action="loginprocess.php">
+    <h1 id="logintitle" for="login">Login</h1>
     <div class="inputs">
-        <input type="text" placeholder="Username"></input>
-        <br>
-        <input type="text" placeholder="Password"></input>
-        <br>
-        <a href="">Forgot Password?</a>
-        <br><br>
-        <input type="submit"></input>
+      <input type="text" placeholder="Username"></input>
+      <br>
+      <input type="text" placeholder="Password"></input>
+      <br>
+      <a href="">Forgot Password?</a>
+      <br><br>
+      <input type="submit"></input>
     </div>
-  
-    </form>
-    
+  </form>
+
 </body>
+
 </html>
--- src/client/sign_up.php
+++ src/client/sign_up.php
@@ -3,55 +3,52 @@
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/style.css" />
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="css/style.css" />
 
-    <style>
-        
-        body {
-    text-align: center;
-    }
-        form {
-    display: inline-block;
-    }
-    #createAcc{
-            margin-top: 17em;
-            color: white;
-            padding-bottom:0.5em;
-        }
-
-    h1{
-        font-size: large;
+  <style>
+    body {
+      text-align: center;
     }
 
-    
+    form {
+      display: inline-block;
+    }
 
+    #createAcc {
+      margin-top: 17em;
+      color: white;
+      padding-bottom: 0.5em;
+    }
 
-        
-    </style>
+    h1 {
+      font-size: large;
+    }
+  </style>
 </head>
 
 <body>
-    
 
-    
 
-    <form name="createAcc" action="createAccprocess.php"> 
-        <h1 id="createAcc" for="createAcc">Create Account</h1>
+
+
+  <form name="createAcc" action="createAccprocess.php">
+    <h1 id="createAcc" for="createAcc">Create Account</h1>
     <div class="inputs">
-        <input type="text" placeholder="email"></input>
-        <br>
-        <input type="text" placeholder="username"></input>
-        <br>
-        <input type="text" placeholder="password"></input>
-        <br><br>
-        <input type="submit"></input>
+      <input type="text" placeholder="email"></input>
+      <br>
+      <input type="text" placeholder="username"></input>
+      <br>
+      <input type="text" placeholder="password"></input>
+      <br><br>
+      <input type="submit"></input>
     </div>
-  
-    </form>
-    
+  </form>
+
 </body>
+
 </html>
