33a1d2db284a6582eae823502223e47ba905d0f4
AmritGoesCrazy <amritpalaujla204@gmail.com>
2021-11-27 00:27:58 -0800

    added bootstrap

--- src/client/login.php
+++ src/client/login.php
@@ -1,55 +1,61 @@
 <?php include('app.php'); ?>
-
-
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="css/style.css" />
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css" />
+    <title>Login</title>
 
-  <style>
-    body {
-      text-align: center;
-    }
 
-    form {
-      display: inline-block;
-    }
+    <style>
+      a,h1{
+        color: white;
+      }
+      #login{
+        margin-top: 10em;
+      }
+      
+      
 
-    #logintitle {
-      margin-top: 17em;
-      color: white;
-      padding-bottom: 0.5em;
-    }
+    </style>
 
-    h1 {
-      font-size: large;
-    }
+  </head>
 
-    a {
-      color: white;
-      font-size: small;
-    }
-  </style>
-</head>
+  <body>
+    
 
-<body>
-  <form name="login" action="loginprocess.php">
-    <h1 id="logintitle" for="login">Login</h1>
-    <div class="inputs">
-      <input type="text" placeholder="Username"></input>
-      <br>
-      <input type="text" placeholder="Password"></input>
-      <br>
-      <a href="">Forgot Password?</a>
-      <br><br>
-      <input type="submit"></input>
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+
+    
+<div class="mt-5">
+    <div class="text-center">
+      <form action="loginprocess.php" style="max-width:300px;margin:auto;" >
+        <h1 class="h3" id="login">Login</h1>
+        <input type="username" id="username" placeholder="username" class="form-control">
+        <input type="password" id="password" placeholder="password" class="form-control">
+        <a href="">Forgot Password?</a>
+
+        <div class="mt-3">
+        <input type="submit"></input>
+        </div>
+
+      </form>
+
     </div>
-  </form>
-
-</body>
-
+</div>
+  
+  </body>
 </html>
--- src/client/sign_up.php
+++ src/client/sign_up.php
@@ -1,54 +1,62 @@
 <?php include('app.php'); ?>
-
-
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="css/style.css" />
-
-  <style>
-    body {
-      text-align: center;
-    }
-
-    form {
-      display: inline-block;
-    }
-
-    #createAcc {
-      margin-top: 17em;
-      color: white;
-      padding-bottom: 0.5em;
-    }
-
-    h1 {
-      font-size: large;
-    }
-  </style>
-</head>
-
-<body>
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css" />
+    <title>Login</title>
 
 
+    <style>
+      a,h1{
+        color: white;
+      }
+      #createAcc{
+        margin-top: 10em;
+      }
+      
+      
 
+    </style>
 
-  <form name="createAcc" action="createAccprocess.php">
-    <h1 id="createAcc" for="createAcc">Create Account</h1>
-    <div class="inputs">
-      <input type="text" placeholder="email"></input>
-      <br>
-      <input type="text" placeholder="username"></input>
-      <br>
-      <input type="text" placeholder="password"></input>
-      <br><br>
-      <input type="submit"></input>
+  </head>
+
+  <body>
+    
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+
+    
+<div class="mt-5">
+    <div class="text-center">
+      <form action="createAccprocess.php" style="max-width:300px;margin:auto;" >
+        <h1 class="h3" id="createAcc">Create Account</h1>
+        <input type="email" id="email" placeholder="email" class="form-control">
+        <input type="username" id="username" placeholder="username" class="form-control">
+        <input type="password" id="password" placeholder="password" class="form-control">
+        
+
+        <div class="mt-3">
+        <input type="submit"></input>
+        </div>
+
+      </form>
+
     </div>
-  </form>
-
-</body>
-
+</div>
+  
+  </body>
 </html>
