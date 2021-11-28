453cba68c81c56de853e2384f9507204c2f778e2
Sebi Unipan <sunipan@gmail.com>
2021-11-27 19:48:08 -0800

    several changes to fix Amrit's mistakes

--- src/client/login.php
+++ src/client/login.php
@@ -1,61 +1,60 @@
 <?php include('app.php'); ?>
 <!doctype html>
 <html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/style.css" />
-    <title>Login</title>
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <!-- Bootstrap CSS -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+  <link rel="stylesheet" href="css/style.css" />
+  <title>Login</title>
 
 
-    <style>
-      a,h1{
-        color: white;
-      }
-      #login{
-        margin-top: 10em;
-      }
-      
-      
+  <style>
+    a,
+    h1 {
+      color: white;
+    }
 
-    </style>
+    #login {
+      margin-top: 10em;
+    }
+  </style>
 
-  </head>
+</head>
 
-  <body>
-    
+<body>
 
-    <!-- Optional JavaScript; choose one of the two! -->
 
-    <!-- Option 1: Bootstrap Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+  <!-- Optional JavaScript; choose one of the two! -->
 
-    <!-- Option 2: Separate Popper and Bootstrap JS -->
-    
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+  <!-- Option 1: Bootstrap Bundle with Popper -->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 
-    
-<div class="mt-5">
-    <div class="text-center">
-      <form action="loginprocess.php" style="max-width:300px;margin:auto;" >
+  <!-- Option 2: Separate Popper and Bootstrap JS -->
+
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+
+
+  <div class="mt-5">
+    <div class="d-flex justify-content-center align-items-center">
+      <form class="col-3" style="max-width: 300px" action="loginprocess.php">
         <h1 class="h3" id="login">Login</h1>
-        <input type="username" id="username" placeholder="username" class="form-control">
-        <input type="password" id="password" placeholder="password" class="form-control">
-        <a href="">Forgot Password?</a>
-
-        <div class="mt-3">
-        <input type="submit"></input>
+        <input type="username" id="username" placeholder="Username" class="form-control mb-2">
+        <input type="password" id="password" placeholder="Password" class="form-control">
+        <a href="#">Forgot Password?</a>
+        <div class="d-flex justify-content-center align-items-center mt-3">
+          <input class="btn btn-dark" type="submit"></input>
         </div>
-
       </form>
 
     </div>
-</div>
-  
-  </body>
+  </div>
+
+</body>
+
 </html>
