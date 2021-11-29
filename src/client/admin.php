4fed91690eae00a4f0dc11dac932c41db559de34
owenspicker <71356125+owenspicker@users.noreply.github.com>
2021-11-29 12:51:23 -0800

    inital admin page

---
+++ src/client/admin.php
@@ -0,0 +1,86 @@
+<?php include('app.php'); ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+  <link rel="stylesheet" href="css/style.css" />
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" integrity="sha512-Fo3rlrZj/k7ujTnHg4CGR2D7kSs0v4LLanw2qksYuRlEzO+tcaEPQogQ0KaoGN26/zrn20ImR1DfuLWnOo7aBA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.1/font/bootstrap-icons.css">
+  <title>Quickscope - Forum For LEETS</title>
+</head>
+<body>
+<br><br>
+
+<div class='container mt-5 pt-5' align ='center'>
+    <h1 class='text-white'>Admin</h1>
+</div>
+
+<div class="container-fluid">
+    <div class="col-lg-10 offset-lg-1">
+            <div class="card-body">
+                    <form id="adminSearch" action="admin.php" method="POST">
+                        <div class="p-1 col-lg-6 offset-lg-3" align ='center'>
+                            <input type="text" class="form-control" name="input" placeholder="Search for user by name, email, or post">
+                            <br>
+                            <button type="submit" class="btn btn-dark">Search</button>
+                        </div>
+                    </form>
+             </div>
+    </div>
+</div>
+<br>
+
+<!--
+    once backend is setup, this will be modified
+-->
+<?php
+
+
+
+if(isset($_POST['input'])){
+
+    //code here to make connection to db, return results
+    $input = $_POST['input'];
+    if($input != ""){
+    echo
+        '<div class="container-fluid mb-5">
+            <div class="col-lg-10 offset-lg-1">
+                <div class="card col-lg-6 offset-lg-3 bg-white" align="center">
+                    <div class="card-body">
+                        <p align="left"><b>User:</b> sample_username</p>
+                        <hr>
+                        <div align="left">
+                            <button class="btn btn-primary">Enable</button>
+                            <button class="btn btn-danger">Disable</button>
+                        </div>
+                        <br>
+                        <p align="left"><b>Post History</b></p>';
+    
+    for($i = 0; $i < 6; $i++){
+        echo
+            '<div class="border border-dark p-2">
+                <p align="left"><b>Title:</b> Quickscope Mania!</p>
+                <div align="right">
+                    <button class="btn btn-dark">Explore</button>
+                </div>
+            </div>';
+    }                     
+
+    echo                    
+                    '</div>
+                </div>
+            </div>
+        </div>';
+}
+}
+
+?>
+
+</body>
+
+
+</html>
