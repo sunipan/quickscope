a35dd16edfd136af25e8383fbf115df558f1408c
Sebi Unipan <sunipan@gmail.com>
2021-12-02 13:14:57 -0800

    removed commented code

--- src/client/my-profile.php
+++ src/client/my-profile.php
@@ -2,38 +2,6 @@
 require('components/head.php');
 require('components/header.php'); ?>
 
-<!-- <div class="card col-6 m-auto mw-md" id="profile-card">
-    <div class="col-md-2 offset-md-1 mt-5">
-      <h1 class='text-black h4'>My Profile</h1>
-      <br>
-      <figure>
-        <img src='img/default_profile_pic.png' height='100px' width='100px'>
-      </figure>
-    </div>
-    <div class="col-8 d-none d-md-block">
-      <br><br><br><br><br>
-      <h4 class="text-black h5">Username: Sample_username <a class="d-flex" href="#">
-          <img src="img/editicon.png" alt="" width="20" height="20" />
-        </a></h4>
-      <br>
-      <h4 class='text-black h5'>Email: Sample_email@email.com<a class="d-flex" href="#">
-          <img src="img/editicon.png" alt="" width="20" height="20" />
-        </a></h4>
-    </div>
-    <br>
-    <div class="offset-md-1">
-      <form action='#'>
-        <button type='submit' class="btn btn-danger btn-sm">Edit Profile Image</button>
-      </form>
-    </div>
-
-    <div class="col-md-10 offset-md-1">
-      <h3 class='text-white'>My Activity</h3>
-      
-      <br> 
-    </div>
-  </div>
-  </div> -->
 <div class="container-fluid post-container">
   <div class="card col-md-8 m-auto px-5 pb-3">
     <div class="row d-flex justify-content-center align-items-center">
