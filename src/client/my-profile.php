9898d7221471628e83e4359c4db8b853137e6324
Sebi Unipan <sunipan@gmail.com>
2021-12-02 13:10:45 -0800

    fixed responsiveness of profile page

--- src/client/my-profile.php
+++ src/client/my-profile.php
@@ -34,8 +34,8 @@ require('components/header.php'); ?>
     </div>
   </div>
   </div> -->
-<div id="profile_card">
-  <div class="card col-md-8 m-auto px-5">
+<div class="container-fluid post-container">
+  <div class="card col-md-8 m-auto px-5 pb-3">
     <div class="row d-flex justify-content-center align-items-center">
       <div class="col-md-4 offset-md-1 mt-2">
         <h4 class='text-black'>My Profile</h4>
@@ -44,21 +44,21 @@ require('components/header.php'); ?>
         </figure>
       </div>
       <div class="col-md-7">
-        <h5 class="text-black fw-bold">Username:</h5>
-        <h5>sample_user
+        <h5 class="text-black profile_user fw-bold">Username:</h5>
+        <h5 class="profile_user">sample_user
           <a class="text-dark text-decoration-none" href="profile-edit.php">
             <i class="bi bi-pencil-square"></i>
           </a>
         </h5>
         </a>
-        <h5 class='text-black fw-bold'>Email:</h5>
-        <h5>sample@email.com <a class="text-dark" href="profile-edit.php">
+        <h5 class='text-black fw-bold profile_email'>Email:</h5>
+        <h5 class="profile_email">sample@email.com <a class="text-dark" href="profile-edit.php">
             <i class="bi bi-pencil-square"></i>
           </a>
         </h5>
       </div>
     </div>
-    <hr class="mx-4 mt-0 mb-2">
+    <hr class="mt-0 mb-2">
     <div class="row">
       <div class="col-md-4 offset-md-1 mt-2">
         <h4 class='text-black'>My Posts</h4>
@@ -67,13 +67,15 @@ require('components/header.php'); ?>
     <?php for ($i = 0; $i < 5; $i++) {
       echo '<div class="row">
       <div class="col-md-10 offset-md-1">
-        <h6 class="text-dark d-flex post_title_profile">Post Title
+        <h6 class="text-dark d-flex">Post Title lorem ipsum dolor sit amet, consectetur adipisicing elit. lorem ips
           <a class="text-dark ms-2" href="post-detail.php">
             <h5><i class="bi bi-pencil-square"></i></h5>
           </a>
         </h6>
       </div>
     </div>';
+      if ($i < 4)
+        echo '<hr class="mt-0 mb-2">';
     } ?>
   </div>
 
