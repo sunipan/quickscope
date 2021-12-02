e2d02a621088a7cb364b0709defad4797954e884
AmritGoesCrazy <amritpalaujla204@gmail.com>
2021-12-02 12:22:00 -0800

    clean up

--- src/client/my-profile.php
+++ src/client/my-profile.php
@@ -15,36 +15,36 @@
 <body>
     <div class="container post-container rounded bg-white border border-dark border border-5">
         <div class="row justify-content-centern offset-lg-2 ml-5">
-            <div class="col-lg-2 offset-lg-1 mt-5">
-                <h1 class='text-black h4'>My Profile</h1>
-                <br>
+            <div class="col-lg-2  mt-5">
+                <h1 class='text-black h4 mb-3'>My Profile</h1>
+                
                 <figure>
                     <img src='img/default_profile_pic.png' height='100px' width='100px'>
                 </figure>
             </div>
-            <div class="col-8 d-none d-lg-block">
-                <br><br><br><br><br>
-                <h4 class="text-black h5">Username: Sample_username <a class="d-flex" href="#">
+            <div class="col-8 d-none d-lg-block mt-4">
+                
+                <h4 class="text-black h5 mt-5 ps-5">Username: Sample_username <a class="d-flex" href="#">
           <img src="img/editicon.png" alt="" width="20" height="20" />
         </a></h4>
-                <br>
-                <h4 class='text-black h5'>Email: Sample_email@email.com<a class="d-flex" href="#">
+                
+                <h4 class='text-black h5 mt-4 ps-5'>Email: Sample_email@email.com<a class="d-flex" href="#">
           <img src="img/editicon.png" alt="" width="20" height="20" />
         </a></h4>
             </div>
-            <br>
-            <div class="offset-lg-1">
+            
+            <div class="">
             <form action='#'>
                 <button type='submit' class="btn btn-danger btn-sm">Edit Profile Image</button>
             </form>
             </div>
 
-            <div class="col-lg-10 offset-lg-1">
+            <div class="col-lg-10 mb-3">
             <h3 class='text-white'>My Activity</h3>
             <?php
             for($i = 0; $i < 3; $i++){
             echo '
-            <div class="card col-lg-12 mb-3 bg-dark" style="max-width: 400px;">
+            <div class="card-fluid col-lg-12 mb-3 bg-dark" >
                 <div class="card-body">
                     <p><b class="text-white">Forum Name - Posted By Sample_username</b></p>
                     <p class="card-text text-white">Sample comment/post</p>
@@ -52,7 +52,7 @@
             </div>';
             }
             ?>
-            <br>
+            
         </div>
         </div>
        
