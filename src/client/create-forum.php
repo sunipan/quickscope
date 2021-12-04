f2d8a13ab59ffe43870b6aaffab9ab90ed00f4aa
Sebi Unipan <sunipan@gmail.com>
2021-12-03 19:16:28 -0800

    create forum changes

--- src/client/create-forum.php
+++ src/client/create-forum.php
@@ -6,7 +6,7 @@ if (!isset($_SESSION['user'])) {
   header('Location: login.php');
 }
 ?>
-<div class="container mt-5 pt-5" align="center">
+<!-- <div class="container mt-5 pt-5" align="center">
   <h1 class="text-white">Create Forum</h1>
 </div>
 <div class="container-fluid">
@@ -26,10 +26,24 @@ if (!isset($_SESSION['user'])) {
         <button id="forumButton" class="btn btn-dark">Create</button>
         <div class="alert alert-danger text-center mt-2 col-lg-4" role="alert" id="login_feedback"></div>
       </div>
-
-
-
     </div>
+  </div>
+</div> -->
+<div class="mt-5">
+  <div class="d-flex justify-content-center align-items-center">
+    <form class="col-3 form-container">
+      <h3 class="text-white text-center" id="login">Create Forum</h3>
+      <input type="text" id="forumTitle" placeholder="Forum Title" class="form-control mb-2">
+      <div class="form-check">
+        <input class="form-check-input" type="checkbox" id="confirm-forum">
+        <label class="form-check-label text-white" for="confirm-forum">
+          I confirm the creation of this forum
+        </label>
+      </div>
+      <button id="forum-button" class="btn btn-dark w-100 mt-3">Create</button>
+      <div class="alert alert-danger text-center mt-2" role="alert" id="forum_error"></div>
+      <div class="alert alert-success text-center mt-2" role="alert" id="forum_success"></div>
+    </form>
   </div>
 </div>
 
